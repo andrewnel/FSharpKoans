95da76a694be2784ea58abbd576a224e5ab2b193
andrewnel <andrew.nel.an@gmail.com>
2019-03-15 01:51:51 +0200

    Finished up to including 09: Advanced Patterns

--- FSharpKoans/AboutDiscriminatedUnions.fs
+++ FSharpKoans/AboutDiscriminatedUnions.fs
@@ -10,6 +10,8 @@ open NUnit.Framework
 
 
 module ``08: The Good Kind of Discrimination`` = 
+    open System
+
     type Subject = // <-- feel free to add your own subjects!
     | Philosophy
     | Linguistics
@@ -27,15 +29,16 @@ module ``08: The Good Kind of Discrimination`` =
     type PostgraduateDegree =
     | Honours of Subject
     | Masters of Subject
+    | PhD of Subject
 
     [<Test>]
     let ``01 A case isn't the same as a type`` () = 
         let aDegree = BSc (Linguistics, ComputerScience)
         let anotherDegree = BPharm
         let philosopherKing = Masters Philosophy
-        aDegree |> should be ofType<FILL_ME_IN> 
-        anotherDegree |> should be ofType<FILL_ME_IN> 
-        philosopherKing |> should be ofType<FILL_ME_IN> 
+        aDegree |> should be ofType<UndergraduateDegree> 
+        anotherDegree |> should be ofType<UndergraduateDegree> 
+        philosopherKing |> should be ofType<PostgraduateDegree> 
    
     [<Test>]
     let ``02 Creating & pattern-matching a discriminated union`` () = 
@@ -44,14 +47,14 @@ module ``08: The Good Kind of Discrimination`` =
             | BSc (_, ComputerScience) | BSc (ComputerScience, _) -> "Good choice!"
             | BSc _ -> "!!SCIENCE!!"
             | BPharm -> "Meh, it's OK."
-            | FILL_ME_IN -> "Money, money, money."
-            | FILL_ME_IN -> "A thinker, eh?"
-        randomOpinion __ |> should equal "Good choice!"
-        randomOpinion __ |> should equal "!!SCIENCE!!"
+            | BCom (Management, _) | BCom (_, Management) -> "Money, money, money."
+            | BA _ -> "A thinker, eh?"
+        randomOpinion (BSc (Mathematics, ComputerScience)) |> should equal "Good choice!"
+        randomOpinion (BSc (Mathematics, Economics)) |> should equal "!!SCIENCE!!"
         randomOpinion (BCom (Management, Economics)) |> should equal "Money, money, money."
         randomOpinion (BCom (Linguistics, Management)) |> should equal "Money, money, money."
         randomOpinion (BA (Linguistics, Philosophy)) |> should equal "A thinker, eh?"
-        randomOpinion __ |> should equal "Meh, it's OK."
+        randomOpinion BPharm |> should equal "Meh, it's OK."
 
     type EquipmentStatus =
     | Available
@@ -60,8 +63,8 @@ module ``08: The Good Kind of Discrimination`` =
 
     [<Test>]
     let ``03 A discriminated union case with associated data is a function`` () =
-        Broken |> should be ofType<FILL_ME_IN>
-        Rented |> should be ofType<FILL_ME_IN>
+        Broken |> should be ofType<int -> EquipmentStatus>
+        Rented |> should be ofType<string -> EquipmentStatus>
 
     type BinaryTree =
     | Empty
@@ -73,5 +76,10 @@ module ``08: The Good Kind of Discrimination`` =
             match x with
             | Empty -> 0
             | Node (_, a, b) -> 1 + max (depth a) (depth b)
-        let a = __ // <-- you may want to spread this over multiple lines and/or let-bindings ...!
+        let a = 
+            let n1 = Node ("E", Empty, Empty)
+            let n2 = Node ("D", Empty,n1)
+            let n3 = Node ("C", Empty, n2)
+            let n4 = Node ("B", Empty, Empty)
+            Node ("A", n4, n3) // <-- you may want to spread this over multiple lines and/or let-bindings ...!
         depth a |> should equal 4
